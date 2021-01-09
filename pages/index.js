76df7886a0a87f4f0ff23b61c9db786104be4d6a
davidcarbn <davidcarbon@web.de>
2021-01-09 22:49:19 +0100

    pages added for test the navigation

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,10 @@ import styles from '../styles/Home.module.css'
 
 const Dashboard = () => {
   return (
-    <h1>Dashboard</h1>
+    <>
+      <h1>Dashboard</h1>
+      
+    </>
   )
 }
 export default Dashboard
---
+++ pages/settings/domain.js
@@ -0,0 +1,4 @@
+const Domain = () => {
+    return (<h1>Settings > Domain Settings</h1>)
+}
+export default Domain
