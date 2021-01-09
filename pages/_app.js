372e91eddf749e6eea1ec7b5fbefb98bb4a75b71
davidcarbn <davidcarbon@web.de>
2021-01-10 00:03:18 +0100

    Added the login route with different Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,26 @@
 import { useMemo } from 'react'
 import DashboardLayout from '../components/DashboardLayout/index.js'
+import AuthProvider from '../context/AuthContext.js'
 import '../styles/globals.css'
 
 
 
-function MyApp({ Component, pageProps }) {
-  
+function MyApp({ Component, pageProps, router }) {
+
   console.log("App rerender")
+  
+  if (router.pathname === '/login') {
+    return (<AuthProvider><Component/>
+      </AuthProvider>)
+  }
+  
   return (
-    <DashboardLayout>
-      <Component {...pageProps} />
-    </DashboardLayout>
+    <AuthProvider>
+      <DashboardLayout>
+        <Component {...pageProps} />
+      </DashboardLayout>
+    </AuthProvider>
+
   )
 }
 
