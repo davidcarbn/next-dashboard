ba0e3d5fec79bbd753e40635dc313cb305ab5e38
davidcarbn <davidcarbon@web.de>
2021-01-09 22:47:25 +0100

    Moved pages object to DashboardLayout Component

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,14 @@
-import DashboardLayout from '../components/DashboardLayout.js'
+import { useMemo } from 'react'
+import DashboardLayout from '../components/DashboardLayout/index.js'
 import '../styles/globals.css'
 
-const pages = [{
-  name: "Dashboard",
-  path: "/",
-  subPages: []
-},{
-  name: "About",
-  path: "/about",
-  subPages: []
-},{
-  name: "Settings",
-  path: "/settings",
-  subPages: [{
-    name: "Domain Settings",
-    path: "/settings/domain"
-  },{
-    name: "Subscription Plans",
-    path: "/settings/subscription"
-  }]
-}]
+
 
 function MyApp({ Component, pageProps }) {
+  
+  console.log("App rerender")
   return (
-    <DashboardLayout pages={pages}>
+    <DashboardLayout>
       <Component {...pageProps} />
     </DashboardLayout>
   )
