32bdfa93aad58f07918859aec63c2ac90b0500fe
davidcarbn <davidcarbon@web.de>
2021-01-12 18:25:01 +0100

    Authcontext changed to fetch user on initital render

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,19 +1,35 @@
-import { createContext, useContext,useState } from "react";
+import { useRouter } from "next/router";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext({
-    user: {},
-    setUser: () => {},
-    isAuth: () => {}
+    user: {
+        name: undefined
+    },
+    setUser: () => { },
+    isAuth: () => { }
 })
 
-const AuthProvider = ({children}) => {
-    const [user,setUser] = useState(null)
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState()
     const isAuth = () => {
         return !!user
     }
+    useEffect(() => {
+        console.log("user fetch")
+        const getUser = async () => {
+            try {
+                const res = await fetch('/api/me')
+                const obj = await res.json()
+                setUser(obj.user)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getUser()
+    },[])
     console.log("AuthProvider rerender")
     return (
-        <AuthContext.Provider value={{user,setUser,isAuth}}>
+        <AuthContext.Provider value={{ user, setUser, isAuth }}>
             {children}
         </AuthContext.Provider>
     )
